ee1f80dca7e2004417a087e63ab3452c1905fdec
baguchi <shutux500@gmail.com>
2026-05-26 16:51:14 +0900

    26.1.2-1.0.2 change rarity

--- src/main/java/baguchi/harpooned/register/ModItems.java
+++ src/main/java/baguchi/harpooned/register/ModItems.java
@@ -12,7 +12,7 @@ import net.neoforged.neoforge.registries.DeferredRegister;
 public class ModItems {
     public static final DeferredRegister.Items ITEMS = DeferredRegister.createItems(Harpooned.MODID);
 
-    public static final DeferredItem<HarpoonCrossbowItem> HARPOON_CROSSBOW = ITEMS.registerItem("harpoon_crossbow", (properties) -> new HarpoonCrossbowItem(properties.durability(532).rarity(Rarity.UNCOMMON).component(DataComponents.CHARGED_PROJECTILES, ChargedProjectiles.EMPTY).enchantable(1).stacksTo(1)));
+    public static final DeferredItem<HarpoonCrossbowItem> HARPOON_CROSSBOW = ITEMS.registerItem("harpoon_crossbow", (properties) -> new HarpoonCrossbowItem(properties.durability(532).rarity(Rarity.RARE).component(DataComponents.CHARGED_PROJECTILES, ChargedProjectiles.EMPTY).enchantable(1).stacksTo(1)));
     public static final DeferredItem<HarpoonItem> IRON_HARPOON = ITEMS.registerItem("iron_harpoon", (properties) -> new HarpoonItem(properties));
 
 }
