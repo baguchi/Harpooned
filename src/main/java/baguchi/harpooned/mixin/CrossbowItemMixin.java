0cf3ebd8e48c70c84e1b76fe34fb79285faff4d7
baguchi <shutux500@gmail.com>
2026-08-30 11:12:11 +0900

    fixed shooting power lower when projectile not iron harpoon

--- src/main/java/baguchi/harpooned/mixin/CrossbowItemMixin.java
+++ src/main/java/baguchi/harpooned/mixin/CrossbowItemMixin.java
@@ -4,11 +4,9 @@ import baguchi.harpooned.item.HarpoonCrossbowItem;
 import baguchi.harpooned.register.ModItemTags;
 import baguchi.harpooned.register.ModItems;
 import net.minecraft.util.Mth;
-import net.minecraft.world.InteractionHand;
 import net.minecraft.world.entity.LivingEntity;
 import net.minecraft.world.item.CrossbowItem;
 import net.minecraft.world.item.ItemStack;
-import net.minecraft.world.item.Items;
 import net.minecraft.world.item.ProjectileWeaponItem;
 import net.minecraft.world.item.component.ChargedProjectiles;
 import net.minecraft.world.item.enchantment.EnchantmentHelper;
@@ -31,9 +29,9 @@ public abstract class CrossbowItemMixin extends ProjectileWeaponItem {
         }
     }
 
-    @Inject(method = "getShootingPower", at = @At("RETURN"))
+    @Inject(method = "getShootingPower", at = @At("RETURN"), cancellable = true)
     private static void getShootingPower(ChargedProjectiles projectiles, CallbackInfoReturnable<Float> cir) {
-        if (!(projectiles.contains(ModItems.IRON_HARPOON.asItem()))) {
+        if ((projectiles.contains(ModItems.IRON_HARPOON.asItem()))) {
             cir.setReturnValue(cir.getReturnValue() - 2);
         }
     }
